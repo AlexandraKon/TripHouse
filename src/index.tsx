dff4a5a1b90e663cc5c46e043c2b167338d23c39
Alexandra Kon <kononenkoalexandra2002@gmail.com>
2022-08-17 17:43:15 +0200

    add changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import '../src/scss/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {store} from '../src/store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <App />
+  <Provider store={store} >
+        <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
